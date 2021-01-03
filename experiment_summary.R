254c0eaff2e156edb064ac63fbb2ddb7514f1225
orzilb <75121220+orzilb@users.noreply.github.com>
2021-01-03 18:43:03 +0200

    Update experiment_summary.R

--- experiment_summary.R
+++ experiment_summary.R
@@ -1,68 +1,73 @@
 ### DESCREAPTION:
-# expSummary() receives GSE experiment id from user and return experiment_summary dataframe object.
-# This script export:
-# 1. The minimal required fields data for a microarray experiment_summary as a csv.
-# 2. RNA sequencing datasets as a csv in case the given GSE contains the dataset.
+# module m includes 2 functions:
+# 1. m$expSummary() 
+#    input: GSE experiment id
+#    return The minimal required fields data for a microarray experiment_summary as a dataframe.
+# 2. m$metadata() 
+#    input: GSE experiment id
+#    return RNA sequencing datasets as a dataframe in case the given GSE contains the dataset, else return NULL.
 
 
-library(rentrez)
-library(GEOquery)
-library(magrittr) 
-library(dplyr)    
-library(XML)
-library(jsonlite)
-library(purrr)
-library(data.table)
 
-## Gets GSE experiment id from user via console
-var = readline("Please enter a 'GSE' experiment id: ")
-if (! startsWith(var,"GSE")) {
-  var = readline("This is not a valied input. Please enter a 'GSE' experiment id: ")
-}
-
-
-expSummary <- function(var) {
+m <- module ({
+  
+  import(rentrez)
+  import(GEOquery)
+  import(magrittr) 
+  import(dplyr)    
+  import(XML)
+  import(jsonlite)
+  import(purrr)
+  import(data.table)
+  
+  
   ##  microarray experiment_summary
-  gds_search <- entrez_search(db="gds", term=paste(var,"[GEO Accession]",sep=""), retmax = 1)
-  s<-entrez_summary(db="gds", id=gds_search$ids)
   
-  df_exp<- data.frame(experiment_id=s[["accession"]],
-                   platform_id=paste("GPL",s[["gpl"]],sep = ""), 
-                   suppfile=s["suppfile"], 
-                   ftplink=s["ftplink"])
-  affy = "FALSE"
-  if (grepl("CEL", s[["suppfile"]], fixed=TRUE)) {
-    affy = "TRUE"
-  }
-  df_exp$affy<-affy
-  
-  rnaseq = "FALSE"
-  if (length(s[["gdstype"]]) > 0) {
-    if (grepl("Expression profiling by high throughput sequencing", s[["gdstype"]], fixed=TRUE)) {
-      rnaseq = "TRUE"
+  expSummary <- function(gse) {
+    gds_search <- entrez_search(db="gds", term=paste(gse,"[GEO Accession]",sep=""), retmax = 1)
+    s<-entrez_summary(db="gds", id=gds_search$ids)
+    
+    summarry_table<- data.frame(experiment_id=s[["accession"]],
+                        platform_id=paste("GPL",s[["gpl"]],sep = ""), 
+                        suppfile=s["suppfile"], 
+                        ftplink=s["ftplink"])
+    affy = "FALSE"
+    if (grepl("CEL", s[["suppfile"]], fixed=TRUE)) {
+      affy = "TRUE"
     }
-  }
-  df_exp$rnaseq<-rnaseq
-  write.csv(df_exp,"experiment_summary.csv", row.names = TRUE)
-  
-  
+    summarry_table$affy<-affy
+    
+    rnaseq = "FALSE"
+    if (length(s[["gdstype"]]) > 0) {
+      if (grepl("Expression profiling by high throughput sequencing", s[["gdstype"]], fixed=TRUE)) {
+        rnaseq = "TRUE"
+      }
+    }
+    summarry_table$rnaseq<-rnaseq
+    
+    
+    return(summarry_table)
+      
+    }
+    
   ## rna sequencing (deep sequencing) datasets if exist in the given GSE
-  sra_search <- entrez_search(db="sra", term=paste(s[["extrelations"]][["targetobject"]],"[sra]",sep=""),retmax=gds_search$count)
   
-  if (length(sra_search$ids) > 0) {
-    ef <-entrez_fetch(db = "sra", id=sra_search$ids, rettype = "runinfo",retmode = "xml",parsed = TRUE)
-    x <- xmlToList(ef)
+  metadata <- function(gse) {
+    gds_search <- entrez_search(db="gds", term=paste(gse,"[GEO Accession]",sep=""), retmax = 1)
+    s<-entrez_summary(db="gds", id=gds_search$ids)
+    sra_search <- entrez_search(db="sra", term=paste(s[["extrelations"]][["targetobject"]],"[sra]",sep=""),retmax=gds_search$count)
     
-    dt_list <- map(x, as.data.table)
-    dt <- rbindlist(dt_list, fill = TRUE, idcol = T)
-    dt<-select(dt,-.id)
-    write.csv(dt,"metadata.csv", row.names = TRUE)
-    
+    # if Metadata exist
+    if (length(sra_search$ids) > 0) {
+      ef <-entrez_fetch(db = "sra", id=sra_search$ids, rettype = "runinfo",retmode = "xml",parsed = TRUE)
+      
+      x <- xmlToList(ef)
+      dt_list <- map(x, as.data.table)
+      sra_meta <- rbindlist(dt_list, fill = TRUE, idcol = T)
+      sra_meta<-select(sra_meta,-.id)
+      return(sra_meta)
+    }
+    # if Metadata not exist
+    return(NULL)
   }
-  return(df_exp)
-}
-
-# experiment_summary object
-experiment_summary<-expSummary(var)
-
-
+})
